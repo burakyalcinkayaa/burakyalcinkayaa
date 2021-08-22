0e4c2f5fffc143b2b881ded2bc41c0dd74eaa88e
burakyalcinkayaa <89360293+burakyalcinkayaa@users.noreply.github.com>
2021-08-23 00:42:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
+const conf = require("./config.json");
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
 });
 
-client.login("ODc5MTA5MjQyMzg3MzIwOTEy.YSK8SA.eNVUlQJdXpLvhFVhTgMMjYFMvH0");
+client.login(conf.token);
